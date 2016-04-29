0b61651aba6420a4dc520b0a840d952f28315565
Michael Medis <TheMedisGroup@gmail.com>
2016-04-29 14:13:28 -0400

    updated facts skill

--- samples/spaceGeek/src/index.js
+++ samples/spaceGeek/src/index.js
@@ -26,20 +26,26 @@ var APP_ID = undefined; //replace with "amzn1.echo-sdk-ams.app.[your-unique-valu
 /**
  * Array containing space facts.
  */
-var SPACE_FACTS = [
-    "A year on Mercury is just 88 days long.",
-    "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-    "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-    "On Mars, the Sun appears about half the size as it does on Earth.",
-    "Earth is the only planet not named after a god.",
-    "Jupiter has the shortest day of all the planets.",
-    "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-    "The Sun contains 99.86% of the mass in the Solar System.",
-    "The Sun is an almost perfect sphere.",
-    "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-    "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-    "The temperature inside the Sun can reach 15 million degrees Celsius.",
-    "The Moon is moving approximately 3.8 cm away from our planet every year."
+var COMPUTER_FACTS = [
+		"The first electronic computer ENIAC weighed more than 27 tons and took up 1800 square feet.",
+    "Only about 10% of the world’s currency is physical money, the rest only exists on computers.",
+    "TYPEWRITER is the longest word that you can write using the letters only on one row of the keyboard of your computer.",
+    "Doug Engelbart invented the first computer mouse in around 1964 which was made of wood.",
+    "There are more than 5000 new computer viruses are released every month.",
+    "Around 50% of all Wikipedia vandalism is caught by a single computer program with more than 90% accuracy.",
+    "If there was a computer as powerful as the human brain, it would be able to do 38 thousand trillion operations per second and hold more than 3580 terabytes of memory.",
+    "The password for the computer controls of nuclear tipped missiles of the U.S was 00000000 for eight years.",
+    "Approximately 70% of virus writers are said to work under contract for organized crime syndicates.",
+    "HP, Microsoft and Apple have one very interesting thing in common – they were all started in a garage.",
+    "An average person normally blinks 20 times a minute, but when using a computer he/she blinks only 7 times a minute.",
+    "The house where Bill Gates lives, was designed using a Macintosh computer.",
+    "The first 1GB hard disk drive was announced in 1980 which weighed about 550 pounds, and had a price tag of $40,000.",
+		"More than 80% of the emails sent daily are spams.",
+		"A group of 12 engineers designed IBM PC and they were called as “The Dirty Dozen”.",
+		"The original name of windows was Interface Manager.",
+		"The first microprocessor created by Intel was the 4004. It was designed for a calculator, and in that time nobody imagined where it would lead.",
+		"IBM 5120 from 1980 was the heaviest desktop computer ever made. It weighed about 105 pounds, not including the 130 pounds external floppy drive.",
+		"Genesis Device demonstration video in Star Trek II: The Wrath of Khan was the the first entirely computer generated movie sequence in the history of cinema. That studio later become Pixar."
 ];
 
 /**
@@ -48,40 +54,40 @@ var SPACE_FACTS = [
 var AlexaSkill = require('./AlexaSkill');
 
 /**
- * SpaceGeek is a child of AlexaSkill.
+ * ComputerGeek is a child of AlexaSkill.
  * To read more about inheritance in JavaScript, see the link below.
  *
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
  */
-var SpaceGeek = function () {
+var ComputerGeek = function () {
     AlexaSkill.call(this, APP_ID);
 };
 
 // Extend AlexaSkill
-SpaceGeek.prototype = Object.create(AlexaSkill.prototype);
-SpaceGeek.prototype.constructor = SpaceGeek;
+ComputerGeek.prototype = Object.create(AlexaSkill.prototype);
+ComputerGeek.prototype.constructor = ComputerGeek;
 
-SpaceGeek.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
-    console.log("SpaceGeek onSessionStarted requestId: " + sessionStartedRequest.requestId
+ComputerGeek.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+    console.log("ComputerGeek onSessionStarted requestId: " + sessionStartedRequest.requestId
         + ", sessionId: " + session.sessionId);
     // any initialization logic goes here
 };
 
-SpaceGeek.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
-    console.log("SpaceGeek onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+ComputerGeek.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+    console.log("ComputerGeek onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
     handleNewFactRequest(response);
 };
 
 /**
  * Overridden to show that a subclass can override this function to teardown session state.
  */
-SpaceGeek.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
-    console.log("SpaceGeek onSessionEnded requestId: " + sessionEndedRequest.requestId
+ComputerGeek.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+    console.log("ComputerGeek onSessionEnded requestId: " + sessionEndedRequest.requestId
         + ", sessionId: " + session.sessionId);
     // any cleanup logic goes here
 };
 
-SpaceGeek.prototype.intentHandlers = {
+ComputerGeek.prototype.intentHandlers = {
     "GetNewFactIntent": function (intent, session, response) {
         handleNewFactRequest(response);
     },
@@ -106,19 +112,18 @@ SpaceGeek.prototype.intentHandlers = {
  */
 function handleNewFactRequest(response) {
     // Get a random space fact from the space facts list
-    var factIndex = Math.floor(Math.random() * SPACE_FACTS.length);
-    var fact = SPACE_FACTS[factIndex];
+    var factIndex = Math.floor(Math.random() * COMPUTER_FACTS.length);
+    var fact = COMPUTER_FACTS[factIndex];
 
     // Create speech output
-    var speechOutput = "Here's your space fact: " + fact;
+    var speechOutput = "Here's your computer fact: " + fact;
 
-    response.tellWithCard(speechOutput, "SpaceGeek", speechOutput);
+    response.tellWithCard(speechOutput, "ComputerGeek", speechOutput);
 }
 
 // Create the handler that responds to the Alexa Request.
 exports.handler = function (event, context) {
-    // Create an instance of the SpaceGeek skill.
-    var spaceGeek = new SpaceGeek();
+    // Create an instance of the ComputerGeek skill.
+    var spaceGeek = new ComputerGeek();
     spaceGeek.execute(event, context);
 };
-
